e8f5477832d641b399e3c53aa0ed22ee161f403f
Anand Yadav <anand.yadav@firmway.in>
2024-04-10 17:00:36 +0530

    Initial commit

---
+++ src/Entrust/Contracts/EntrustPermissionInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace Firmway\Entrust\Contracts;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Firmway\Entrust
+ */
+
+interface EntrustPermissionInterface
+{
+
+    /**
+     * Many-to-Many relations with role model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles();
+}
